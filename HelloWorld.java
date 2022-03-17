03539a66cf06a8ce5bad869b77b6b192e07a4b6b
jmstechhome <jmstechhome16@gmail.com>
2022-03-17 02:10:59 +0000

    added one moreline

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
       System.out.println("Hello, Guys...Happy Holi");
+      System.out.println("Happly long weekend");
   }
 }
