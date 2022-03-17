e47fb473234694e2d7a5c160f073fb317dcce273
jmstechhome16 <100742284+jmstechhome16@users.noreply.github.com>
2022-03-17 07:56:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
   public static void main(String[] args) {
       System.out.println("Hello, Guys...Happy Holi");
       System.out.println("Happly long weekend");
+    System.out.println("Happly long weekend");
+    System.out.println("Happly long weekend");
+    System.out.println("Happly long weekend");
+    System.out.println("Happly long weekend");
+    System.out.println("Happly long weekend");
   }
 }
